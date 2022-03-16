99da9509a27ae7e97a792d3ecc66d3fa21bf89a8
mberkayakar <m.berkay.akar@gmail.com>
2022-03-16 23:10:43 +0300

    inital commit for JS

---
+++ JavaScript/JavaScript/firstapp.js
@@ -0,0 +1,3 @@
+alert("Merhaba Berkay Akar Ben firstapp ismindeki external dosyayım")
+
+document.getElementById("h2").style.color = "blue";
---
+++ JavaScript/JavaScript/secondapp.js
@@ -0,0 +1,32 @@
+document.getElementById("h2").style.color = "red";
+alert("AHADA KIRMIZI")
+
+alert("Console kısmında belirlediğiniz sayı isimli bir değişlkende belirlediğiniz veri bulunmaktadır")
+var a = "Ahmet Hakan Hüseyin"
+console.log(a);
+
+console.log(true)
+console.log(false)
+
+
+// Array Tanımlama
+console.log([1,2,3,4,5,6,7,8,9])
+// Bu şekilde 9 elemanlı bir array tanımlanmış bulunmaktadır. 
+
+
+var dizi = [1,2,3,4,5,6,7,8,9]
+
+for(i = 0; i < dizi.length; i++) {
+    // alert(dizi[i]);
+}
+
+// bir değişkenin veri tipini öğrenme
+console.log(typeof dizi)
+
+// konsola bir uyarı yazısı yazdırtmak için (ünlem işareti ile bir uyarı gibi çıkar)
+
+console.warn("Bu bir uyarı yazisisdir. ")
+
+
+// konsola bir hata  yazısı yazdırtmak için (ünlem işareti ile bir hata gibi kırmızı şekilde çıkar)
+console.error("Bu bir uyarı yazisisdir. ")
